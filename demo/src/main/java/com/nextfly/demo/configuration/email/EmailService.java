3620a6bad4e47e9bd2a4cce01b6ddda916f2a17d
Filippo Lippolis <63558212+fil23@users.noreply.github.com>
2025-01-14 18:09:39 +0100

    send email

--- demo/src/main/java/com/nextfly/demo/configuration/email/EmailService.java
+++ demo/src/main/java/com/nextfly/demo/configuration/email/EmailService.java
@@ -5,10 +5,12 @@ import java.util.Random;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 
+@Service
 public class EmailService {
     private final JavaMailSender javaMailSender;
     private final String CARATTERI = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890";
